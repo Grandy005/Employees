189e991b4efb9bdefb8520c0f4a592243b154dcd
Grandy005 <attilaagoston2005@gmail.com>
2024-09-16 13:23:59 +0200

    Created Employee class

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Employees
 {
+    class Employee 
+    {
+        private int id;
+        private string name;
+        private int age;
+        private int salary;
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public int Salary { get => salary; set => salary = value; }
+
+        public Employee(int id, string name, int age, int salary)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
